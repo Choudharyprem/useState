558a564dc8993d2e9a87d2ff909e004c5eae9594
Choudharyprem <choudharypremchand16@gmail.com>
2022-06-06 21:38:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-rfmlme
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-rfmlme)
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React,{useState} from "react";
+import "./style.css";
+
+
+export default function App() {
+  let [test,setTest] =useState(0)
+
+  
+function increment(){
+  setTest(test+1)
+}
+function decrement(){
+  setTest(test-1)
+}
+function reset(){
+  setTest(0)
+}
+  return (
+    <div>
+     <button onClick={increment}>+</button>
+      <div>{test}</div>
+      <button onClick={decrement}>-</button>
+      <button onClick={reset}>reset</button>
+    </div>
+  );
+}
